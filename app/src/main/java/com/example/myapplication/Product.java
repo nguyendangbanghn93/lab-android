57bb795b260ed324f59dc049a5a5aba48922fe47
nguyendangbanghn93 <nguyendangbanghn@gmail.com>
2021-09-22 21:08:00 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/Product.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+public class Product {
+    private String name;
+    private String price;
+    private int image;
+
+    public Product()
+    {
+
+    }
+
+    public Product(String name, String price, int image) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
